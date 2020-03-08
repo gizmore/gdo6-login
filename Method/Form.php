70cb2a0acba2a740cee080dd15911a7623810c52
gizmore <gizmore@wechall.net>
2020-03-08 03:05:40 +0100

    6.10-r6005-nested-gdo-forms

--- Method/Form.php
+++ Method/Form.php
@@ -48,7 +48,7 @@ final class Form extends MethodForm
 			$form->addField(GDT_Captcha::make());
 		}
 		$form->addField(GDT_Submit::make()->label('btn_login'));
-		$form->addField(GDT_AntiCSRF::make());
+// 		$form->addField(GDT_AntiCSRF::make());
 // 		$form->addField(GDT_Button::make('btn_recovery')->href(href('Recovery', 'Form')));
 	
 		GDT_Hook::callHook('LoginForm', $form);
