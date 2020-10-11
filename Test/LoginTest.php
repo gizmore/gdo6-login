f062e4f4b68493d3b6f8e8969eeee486afc99bdb
gizmore <gizmore@wechall.net>
2020-10-11 16:07:12 +0200

    6.10-r9101_unit

---
+++ Test/LoginTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace GDO\Login\Test;
+
+use PHPUnit\Framework\TestCase;
+use GDO\Tests\MethodTest;
+
+final class LoginTest extends TestCase
+{
+    public function testLoginBlocked()
+    {
+        MethodTest::class
+    }
+    
+}
