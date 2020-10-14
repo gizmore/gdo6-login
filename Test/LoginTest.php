e9b767205eac65466f9621cc2ef870e94d9dccf1
gizmore <gizmore@wechall.net>
2020-10-14 12:32:08 +0200

    6.10-r9048_bootstrap_theme

--- Test/LoginTest.php
+++ Test/LoginTest.php
@@ -3,12 +3,21 @@ namespace GDO\Login\Test;
 
 use PHPUnit\Framework\TestCase;
 use GDO\Tests\MethodTest;
+use GDO\Login\Method\Form;
 
 final class LoginTest extends TestCase
 {
+    
     public function testLoginBlocked()
     {
-        MethodTest::class
+        $parameters = array(
+            'login' => 'gizmore',
+            'password' => 'incorrect',
+            'bindip' => '0',
+        );
+        $response = MethodTest::make()->method(Form::make())->parameters($parameters)->execute();
+        
+        var_dump($response);
     }
     
 }
